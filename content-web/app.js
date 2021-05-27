86ad6d39b294bc3e1c0a05380fe4ac53ca438def
kenny-tan <tkl.sh@icloud.com>
2021-05-27 11:54:46 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("c504ce9c-a9d1-4370-8e83-13d787f1eb92");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://23.99.10.186:3001";
 
